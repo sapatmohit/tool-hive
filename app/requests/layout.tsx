d0a5fdf0b6faddcb854566ded49a360ef0ad01c5
sushrutingle <sushrutingle7@gmail.com>
2026-03-03 19:01:25 +0530

    Revert "Authentication"

--- app/requests/layout.tsx
+++
@@ -1,9 +0,0 @@
-import ProtectedLayout from "@/components/ProtectedLayout";
-
-export default function RequestsLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <ProtectedLayout>{children}</ProtectedLayout>;
-}
